76858ad5377672c47cc4c7d95e970bbe17037fc3
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-22 17:07:23 -0600

    minor card bug fixes

--- js/index.js
+++ js/index.js
@@ -7,29 +7,30 @@ window.addEventListener("load", function() {
   }
 
   const cards = [
-    ((join = new Card(`imgs/assets/sign up.png`, `Quick sign up`)),
+    (join = new Card(`imgs/assets/sign up.png`, `Quick sign up`)),
     (search = new Card(`imgs/assets/search.png`, `Easy search function`)),
     (video = new Card(`imgs/assets/video.png`, `Upload videos and photos`)),
-    (vote = new Card(`imgs/assets/vote.png`, `Vote for the best content`)))
+    (vote = new Card(`imgs/assets/vote.png`, `Vote for the best content`))
   ];
 
   const mobileImg = document.querySelector(".cards .card img");
-  console.log(mobileImg);
   const mobileP = document.querySelector(".cards .card p");
   const mobileNext = document.querySelector(".cards .mobile .next");
   const mobileBack = document.querySelector(".cards .mobile .back");
 
-  let current = 0;
-  if (current == cards.length) {
-    current = 0;
+  let count = 0;
+
+  mobileNext.onclick = function() {
+    return (count = count + 1);
+  };
+  mobileBack.onclick = function() {
+    return (count = count - 1);
+  };
+
+  if (count == cards.length) {
+    count = 0;
   }
 
-  mobileImg.src = `${cards[current].img}`;
-  mobileP.textContent = `${cards[current].p}`;
-
-  mobileNext.addEventListener("click", () => {
-    current += 1});
-  mobileBack.addEventListener("click", () => {
-    current -= 1;
-  });
+  mobileImg.src = `${cards[count].img}`;
+  mobileP.textContent = `${cards[count].p}`;
 });
